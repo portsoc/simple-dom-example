d9a079dcda6871efab0d822a977031ea14f55171
Rich Boakes <rjb@port.ac.uk>
2019-11-18 13:35:35 +0000

    go live 1

--- create/create.js
+++ create/create.js
@@ -8,13 +8,32 @@ console.log(myElem);
 // notice that we have created it, but it
 // does not appear *in* the document yet.
 
+// now let's add the paragraph to the document
+document.body.appendChild(myElem);
+
+
 // we created a p element.  Paragraphs can
 // contain text, so let's add some using the
 // special property "textContent".
 myElem.textContent = "hello world";
 console.log(myElem);
 
-// now let's add the paragraph to the document
-document.body.appendChild(myElem);
 
+
+
+function updateTheTime() {
+  theTime.textContent = (new Date()).toISOString();
+}
+
+// now add a paragraph that tells the time
+const theTime = document.createElement("p");
+document.body.appendChild(theTime);
+updateTheTime();
+
+// now add a button to the page
+const myButton = document.createElement("button");
+myButton.textContent = "Update";
+document.body.appendChild(myButton);
+
+myButton.addEventListener("click", updateTheTime);
 
